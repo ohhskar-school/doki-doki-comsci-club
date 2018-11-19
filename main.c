818846730bb6ba8f22339ca09e0d3d1eb6a2aeaa
Oscar Vian Valles <oscarvianvalles@gmail.com>
2018-11-19 09:33:01 +0800

    initial system

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <math.h>
+
+// int eventSwitcher(int event){
+//   int nextAction
+  
+//   switch (event)
+//   {
+//     case 1:
+//       break;
+  
+//     default:
+//       printf("Error 1: Undefined Event Number was entered in the function eventSwitcher()")
+//       break;
+//   }
+//   return (nextAction)
+// }
+
+// int event1(){
+
+// }
+int main(int argc, char const *argv[]){
+  int interestPointsA;
+  int interestPointsB;
+  int interestPointsC;
+  int heartsA;
+  int heartsB;
+  int heartsC;
+
+  heartsA = floor(interestPointsA/100);
+  heartsB = floor(interestPointsB/100);
+  heartsC = floor(interestPointsC/100);
+
+  return 0;
+}
+
+
+/* Things to do
+* Quest System
+* Love System
+* Interest Gauge
+* Player Creation System
+* 
+*/
