a74e6cd20ab746420417aeb7a9e0d6e15c6cc406
PatTumulak <tumulakpatricia@gmail.com>
2018-12-11 16:02:11 +0800

    test

--- gameEvents.h
+++ gameEvents.h
@@ -4,5 +4,19 @@
 
 gameInfo mainMenu(gameInfo _eventInfo);
 gameInfo dayOne(gameInfo _eventInfo);
+gameInfo onePathOne(gameInfo _eventInfo);
+gameInfo onePathTwo(gameInfo _eventInfo);
+gameInfo onePathThree(gameInfo _eventInfo);
+gameInfo dayOneCont(gameInfo _eventInfo);
+gameInfo dayOneContTwo(gameInfo _eventInfo);
+gameInfo dayTwo(gameInfo _eventInfo);
+gameInfo twoPathOne(gameInfo _eventInfo);
+gameInfo twoPathTwo(gameInfo _eventInfo);
+gameInfo clubOne(gameInfo _eventInfo);
+gameInfo clubOneCont(gameInfo _eventInfo);
+gameInfo clubOneContTwo(gameInfo _eventInfo);
+gameInfo clubOneContThree(gameInfo _eventInfo);
+gameInfo clubOneContFour(gameInfo _eventInfo);
+gameInfo clubOneContFive(gameInfo _eventInfo);
 
 #endif
